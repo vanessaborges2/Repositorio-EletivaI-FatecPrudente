08211efa5e19be3bc87628f70c3f0cdd961acd95
Vanessa <vanessa.borges2@fatec.sp.gov.br>
2025-08-14 20:02:10 -0300

    Primeiro código em PHP

---
+++ 14-08/exemplo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo</title>
+</head>
+<body>
+    <?php
+        $dia = date("d");
+        //Comentário
+        # Comentário
+        /*
+            Comentário
+        */
+    ?>
+    <h1> Hoje é dia <?= $dia ?> de Agosto de 2025 </h1>
+
+</body>
+</html>
