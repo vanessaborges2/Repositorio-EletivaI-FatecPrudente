5e0cc31bce5c8aea9e965fdabc23ecf24cf80b82
Vanessa <vanessa.borges2@fatec.sp.gov.br>
2025-08-28 20:19:53 -0300

    Aula 28-08

---
+++ 28-08/estrutura2.php
@@ -0,0 +1,14 @@
+<?php
+
+    include("cabecalho.php");
+
+    $valor = 10;
+
+    if($valor > 10){
+        echo "Valor maior que 10";
+    } elseif($valor < 10){
+        echo "Valor menor que 10";
+    } else 
+        echo "Igual a 10!";
+
+    include("rodape.php");
