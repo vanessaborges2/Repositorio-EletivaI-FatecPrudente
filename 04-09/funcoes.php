a5683c02cbebbaca614e5cf3109f5b880fa9d872
Vanessa <vanessa.borges2@fatec.sp.gov.br>
2025-09-04 19:47:23 -0300

    AUla 04-09 (2)

---
+++ 04-09/funcoes.php
@@ -0,0 +1,6 @@
+<?php
+    $nome = "Vanessa";
+    echo "<p>Todas em maiúsculo: ".strtoupper($nome)."</p>";
+    echo "<p>Todas em minúsculo: ".strtolower($nome)."</p>";
+    echo "<p>Qtd. de caracteres:".strlen($nome)."</p>";
+
